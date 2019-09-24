06420bb431bb9a35d8e3c156c2242df3e63f8375
Fearchar MacLean <fearchar.maclean@gmail.com>
2019-09-24 17:18:20 +0100

    Started on Hero

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import TryingHooks from './components/TryingHooks'
+import Hero from './components/Hero'
 
 class App extends React.Component {
 
   render () {
     return (
-      <TryingHooks />
+      <Hero />
     )
   }
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <h1>Fearchar</h1>
+      <h2>MacLean</h2>
+      <div>
+        <p>github: blah</p>
+        <p>linkedIn: blah</p>
+        <p>email: blah</p>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
