e2248eca57d935ad43fae79cb91d0726feda5f75
Fearchar MacLean <fearchar.maclean@gmail.com>
2019-09-24 16:10:23 +0100

    Initial commit.

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+
+  render () {
+    return (
+      <h1>Profile still in developement. Watch this space!</h1>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
