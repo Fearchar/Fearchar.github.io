7823a774628ffeae38dd32096468f465d22b577d
Fearchar MacLean <fearchar.maclean@gmail.com>
2019-10-02 16:18:54 +0100

    Entered and styled contact info

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,9 +8,18 @@ const Hero = () => {
       <p>with a passion for tabletop gaming,</p>
       <p>music and literature</p>
       <div>
-        <p>github: blah</p>
-        <p>linkedIn: blah</p>
-        <p>email: blah</p>
+        <a href="https://github.com/Fearchar">
+          <i className="devicon-github-plain"></i>
+          <p>/Fearchar</p>
+        </a>
+        <a href="https://www.linkedin.com/in/fearchar/">
+          <i className="fab fa-linkedin"></i>
+          <p>/fearchar</p>
+        </a>
+        <a href="mailto: fearchar.maclean@gmail.com">
+          <i className="fas fa-envelope"></i>
+          <p>fearchar.maclean@gmail.com</p>
+        </a>
       </div>
     </div>
   )
