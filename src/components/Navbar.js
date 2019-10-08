e1e913357c5a79b75f51f423132057557335bf2e
Fearchar MacLean <fearchar.maclean@gmail.com>
2019-10-08 15:01:45 +0100

    Nav burger menu at mobile width created.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Navbar = ({ displayNav }) => {
+  const [navOpen, setNavOpen] = useState(false)
   return (
-    <div className={`${displayNav && 'full'}`}>
+    <div className={`${displayNav && 'active'} ${ navOpen && 'open'}`}>
+      <i
+        className="fas fa-bars"
+        onClick={() => navOpen ? setNavOpen(false) : setNavOpen(true)}
+      ></i>
       <a href="#hero">Fearchar MacLean</a>
       <a href="#bio">Bio</a>
       <a href="#expirience">Expirience</a>
@@ -13,3 +18,29 @@ const Navbar = ({ displayNav }) => {
 }
 
 export default Navbar
+
+/*    <div className={`${displayNav && 'active'}`}>
+      <a
+        className={`${asActive && 'active'}`}
+        href="#hero"
+      >Fearchar MacLean</a>
+      <a
+        className={`${asActive && 'active'}`}
+        href="#bio"
+      >Bio</a>
+      <a
+        className={`${asActive && 'active'}`}
+        href="#expirience"
+      >Expirience</a>
+      <a
+        className={`${asActive && 'active'}`}
+      >Projects</a>
+      <a
+        className={`${asActive && 'active'}`}
+      >Interests</a>
+      <i
+        className="fas fa-bars"
+        onClick={() => asActive ? setAsActive(false) : setAsActive(true)}
+      ></i>
+    </div>
+*/
