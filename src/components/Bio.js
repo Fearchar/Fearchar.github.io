700b711061df3615850efbd67bf6c0cb4dc91b78
Fearchar MacLean <fearchar.maclean@gmail.com>
2019-10-13 14:56:32 +0100

    Expirience section styling improvements

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -9,26 +9,28 @@ const Bio = () => {
 // !!! Add FM image to content folder
   return (
     <section id="bio">
-      <div className="section-third subsection">
-        <TabAnchor href="https://github.com/fearchar">
-          <img src="https://i.imgur.com/2siaQqK.jpg" alt="Fearchar MacLean"/>
-        </TabAnchor>
-      </div>
-      <div className="section-third subsection">
-        <h3>My Bio</h3>
-        <p>
-          {bioText}
-        </p>
-      </div>
-      <div className="section-third subsection">
-        <h3>Tech</h3>
-        <div className="techs">
-          {knownTechs.map(tech =>
-            <Technology
-              key={tech.name}
-              {...tech}
-            />
-          )}
+      <div className="container">
+        <div className="section-third subsection">
+          <TabAnchor href="https://github.com/fearchar">
+            <img src="https://i.imgur.com/2siaQqK.jpg" alt="Fearchar MacLean"/>
+          </TabAnchor>
+        </div>
+        <div className="section-third subsection">
+          <h3>My Bio</h3>
+          <p>
+            {bioText}
+          </p>
+        </div>
+        <div className="section-third subsection">
+          <h3>Tech</h3>
+          <div className="techs">
+            {knownTechs.map(tech =>
+              <Technology
+                key={tech.name}
+                {...tech}
+              />
+            )}
+          </div>
         </div>
       </div>
     </section>
