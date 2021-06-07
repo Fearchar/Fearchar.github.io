c39cef4d125560f93467139c6de12acbbcc106bb
Fearchar MacLean <46941430+Fearchar@users.noreply.github.com>
2021-06-07 16:10:22 +0100

    Basic banner done

--- src/components/page/main/shared/Banner.js
+++ src/components/page/main/shared/Banner.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
+import styles from './Banner.css'
+
 export function Banner({ id, heading }) {
   return (
-    <div className="banner">
-      <div className="nav-offset" id={id} />
+    <div className={styles.main} id={id}>
       <h2>{heading}</h2>
     </div>
   )
