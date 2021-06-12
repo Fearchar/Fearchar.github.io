d0eb0f874800a8908bfe1d5ba6d04bd2bd357f19
Fearchar MacLean <46941430+Fearchar@users.noreply.github.com>
2021-06-12 14:40:00 +0100

    Using capital case for constants in tech.js

--- src/content/tech.js
+++ src/content/tech.js
@@ -6,8 +6,8 @@ class Tech {
     this.aboutExclude = aboutExclude
   }
 }
-
-const techNames = {
+/* todo: fm - labele all constants as constants?*/
+const TECH_NAMES = {
   js: 'JavaScript',
   react: 'React',
   mongodb: 'MongoDB',
@@ -26,35 +26,58 @@ const techNames = {
   heroku: 'Heroku'
 }
 
-/* todo: fm - change to knownTechs*/
-export const allKnownTechs = {
-  [techNames['js']]: new Tech(techNames['js'], 'javascript-plain', 10),
-  [techNames['react']]: new Tech(techNames['react'], 'react-original', 8),
-  [techNames['mongodb']]: new Tech(techNames['mongodb'], 'mongodb-plain', 6),
-  [techNames['mocha']]: new Tech(techNames['mocha'], 'mocha-plain', 5),
-  [techNames['bable']]: new Tech(techNames['bable'], 'babel-plain', 7),
-  [techNames['webpack']]: new Tech(techNames['webpack'], 'webpack-plain', 7.3),
-  [techNames['python']]: new Tech(techNames['python'], 'python-plain', 3),
-  [techNames['node']]: new Tech(techNames['node'], 'nodejs-plain', 7),
-  [techNames['html']]: new Tech(techNames['html'], 'html5-plain', 9),
-  [techNames['css']]: new Tech(techNames['css'], 'css3-plain', 9),
-  [techNames['scss']]: new Tech(techNames['scss'], 'sass-original', 8),
-  [techNames['django']]: new Tech(techNames['django'], 'django-plain', 3),
-  [techNames['express']]: new Tech(techNames['express'], 'express-original', 6),
-  [techNames['git']]: new Tech(techNames['git'], 'git-plain', 8),
-  [techNames['postgres']]: new Tech(
-    techNames['postgres'],
+/* todo: fm - change to KNOWN_TECHS*/
+export const ALL_KNOWN_TECHS = {
+  [TECH_NAMES['js']]: new Tech(TECH_NAMES['js'], 'javascript-plain', 10),
+  [TECH_NAMES['react']]: new Tech(TECH_NAMES['react'], 'react-original', 8),
+  [TECH_NAMES['mongodb']]: new Tech(TECH_NAMES['mongodb'], 'mongodb-plain', 6),
+  [TECH_NAMES['mocha']]: new Tech(TECH_NAMES['mocha'], 'mocha-plain', 5),
+  [TECH_NAMES['bable']]: new Tech(TECH_NAMES['bable'], 'babel-plain', 7),
+  [TECH_NAMES['webpack']]: new Tech(
+    TECH_NAMES['webpack'],
+    'webpack-plain',
+    7.3
+  ),
+  [TECH_NAMES['python']]: new Tech(TECH_NAMES['python'], 'python-plain', 3),
+  [TECH_NAMES['node']]: new Tech(TECH_NAMES['node'], 'nodejs-plain', 7),
+  [TECH_NAMES['html']]: new Tech(TECH_NAMES['html'], 'html5-plain', 9),
+  [TECH_NAMES['css']]: new Tech(TECH_NAMES['css'], 'css3-plain', 9),
+  [TECH_NAMES['scss']]: new Tech(TECH_NAMES['scss'], 'sass-original', 8),
+  [TECH_NAMES['django']]: new Tech(TECH_NAMES['django'], 'django-plain', 3),
+  [TECH_NAMES['express']]: new Tech(
+    TECH_NAMES['express'],
+    'express-original',
+    6
+  ),
+  [TECH_NAMES['git']]: new Tech(TECH_NAMES['git'], 'git-plain', 8),
+  [TECH_NAMES['postgres']]: new Tech(
+    TECH_NAMES['postgres'],
     'postgresql-plain',
     3
   ),
-  [techNames['heroku']]: new Tech(techNames['heroku'], 'heroku-original', 5)
+  [TECH_NAMES['heroku']]: new Tech(TECH_NAMES['heroku'], 'heroku-original', 5)
 }
 
-export const knownTechs = Object.keys(allKnownTechs).map(
-  (key) => allKnownTechs[key]
+export const KNOWN_TECHS = Object.keys(ALL_KNOWN_TECHS).map(
+  (key) => ALL_KNOWN_TECHS[key]
 )
 
-export const aboutTechs = [knownTechs['JavaScript']]
+/* todo: fm - reorder*/
+export const ABOUT_TECHS = [
+  ALL_KNOWN_TECHS[TECH_NAMES['js']],
+  ALL_KNOWN_TECHS[TECH_NAMES['react']],
+  ALL_KNOWN_TECHS[TECH_NAMES['mongodb']],
+  ALL_KNOWN_TECHS[TECH_NAMES['bable']],
+  ALL_KNOWN_TECHS[TECH_NAMES['webpack']],
+  ALL_KNOWN_TECHS[TECH_NAMES['python']],
+  ALL_KNOWN_TECHS[TECH_NAMES['node']],
+  ALL_KNOWN_TECHS[TECH_NAMES['html']],
+  ALL_KNOWN_TECHS[TECH_NAMES['css']],
+  ALL_KNOWN_TECHS[TECH_NAMES['django']],
+  ALL_KNOWN_TECHS[TECH_NAMES['express']],
+  ALL_KNOWN_TECHS[TECH_NAMES['git']],
+  ALL_KNOWN_TECHS[TECH_NAMES['postgres']]
+]
 
 /* todo: fm - delete*/
-export const otherTechs = [new Tech('Bulma', null, null)]
+export const OTHER_TECHS = [new Tech('Bulma', null, null)]
