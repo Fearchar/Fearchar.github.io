80b404e046840cd34865a06fbed439f9bf2f1bec
Fearchar MacLean <46941430+Fearchar@users.noreply.github.com>
2021-06-12 15:05:24 +0100

    amneded ABOUT_TECHS

--- src/content/tech.js
+++ src/content/tech.js
@@ -23,61 +23,51 @@ const TECH_NAMES = {
   express: 'Express',
   git: 'Git',
   postgres: 'PostgreSQL',
-  heroku: 'Heroku'
+  heroku: 'Heroku',
+  sqlalchemy: 'SQLAlchemy'
 }
 
 /* todo: fm - change to KNOWN_TECHS*/
-export const ALL_KNOWN_TECHS = {
-  [TECH_NAMES['js']]: new Tech(TECH_NAMES['js'], 'javascript-plain', 10),
-  [TECH_NAMES['react']]: new Tech(TECH_NAMES['react'], 'react-original', 8),
-  [TECH_NAMES['mongodb']]: new Tech(TECH_NAMES['mongodb'], 'mongodb-plain', 6),
-  [TECH_NAMES['mocha']]: new Tech(TECH_NAMES['mocha'], 'mocha-plain', 5),
-  [TECH_NAMES['bable']]: new Tech(TECH_NAMES['bable'], 'babel-plain', 7),
-  [TECH_NAMES['webpack']]: new Tech(
-    TECH_NAMES['webpack'],
-    'webpack-plain',
-    7.3
-  ),
-  [TECH_NAMES['python']]: new Tech(TECH_NAMES['python'], 'python-plain', 3),
-  [TECH_NAMES['node']]: new Tech(TECH_NAMES['node'], 'nodejs-plain', 7),
-  [TECH_NAMES['html']]: new Tech(TECH_NAMES['html'], 'html5-plain', 9),
-  [TECH_NAMES['css']]: new Tech(TECH_NAMES['css'], 'css3-plain', 9),
-  [TECH_NAMES['scss']]: new Tech(TECH_NAMES['scss'], 'sass-original', 8),
-  [TECH_NAMES['django']]: new Tech(TECH_NAMES['django'], 'django-plain', 3),
-  [TECH_NAMES['express']]: new Tech(
-    TECH_NAMES['express'],
-    'express-original',
-    6
-  ),
-  [TECH_NAMES['git']]: new Tech(TECH_NAMES['git'], 'git-plain', 8),
-  [TECH_NAMES['postgres']]: new Tech(
-    TECH_NAMES['postgres'],
-    'postgresql-plain',
-    3
-  ),
-  [TECH_NAMES['heroku']]: new Tech(TECH_NAMES['heroku'], 'heroku-original', 5)
-}
-
-export const KNOWN_TECHS = Object.keys(ALL_KNOWN_TECHS).map(
-  (key) => ALL_KNOWN_TECHS[key]
-)
+export const KNOWN_TECHS = [
+  new Tech(TECH_NAMES['js'], 'javascript-plain', 10),
+  new Tech(TECH_NAMES['react'], 'react-original', 8),
+  new Tech(TECH_NAMES['mongodb'], 'mongodb-plain', 6),
+  new Tech(TECH_NAMES['mocha'], 'mocha-plain', 5),
+  new Tech(TECH_NAMES['bable'], 'babel-plain', 7),
+  new Tech(TECH_NAMES['webpack'], 'webpack-plain', 7.3),
+  new Tech(TECH_NAMES['python'], 'python-plain', 3),
+  new Tech(TECH_NAMES['node'], 'nodejs-plain', 7),
+  new Tech(TECH_NAMES['html'], 'html5-plain', 9),
+  new Tech(TECH_NAMES['css'], 'css3-plain', 9),
+  new Tech(TECH_NAMES['scss'], 'sass-original', 8),
+  new Tech(TECH_NAMES['django'], 'django-plain', 3),
+  new Tech(TECH_NAMES['express'], 'express-original', 6),
+  new Tech(TECH_NAMES['git'], 'git-plain', 8),
+  new Tech(TECH_NAMES['postgres'], 'postgresql-plain', 3),
+  new Tech(TECH_NAMES['heroku'], 'heroku-original', 5),
+  new Tech(
+    TECH_NAMES['sqlalchemy'],
+    'devicon-sqlalchemy-original-wordmark colored',
+    5
+  )
+]
 
 /* todo: fm - reorder*/
 export const ABOUT_TECHS = [
-  ALL_KNOWN_TECHS[TECH_NAMES['js']],
-  ALL_KNOWN_TECHS[TECH_NAMES['react']],
-  ALL_KNOWN_TECHS[TECH_NAMES['mongodb']],
-  ALL_KNOWN_TECHS[TECH_NAMES['bable']],
-  ALL_KNOWN_TECHS[TECH_NAMES['webpack']],
-  ALL_KNOWN_TECHS[TECH_NAMES['python']],
-  ALL_KNOWN_TECHS[TECH_NAMES['node']],
-  ALL_KNOWN_TECHS[TECH_NAMES['html']],
-  ALL_KNOWN_TECHS[TECH_NAMES['css']],
-  ALL_KNOWN_TECHS[TECH_NAMES['django']],
-  ALL_KNOWN_TECHS[TECH_NAMES['express']],
-  ALL_KNOWN_TECHS[TECH_NAMES['git']],
-  ALL_KNOWN_TECHS[TECH_NAMES['postgres']]
-]
+  TECH_NAMES['js'],
+  TECH_NAMES['react'],
+  TECH_NAMES['mongodb'],
+  TECH_NAMES['bable'],
+  TECH_NAMES['webpack'],
+  TECH_NAMES['python'],
+  TECH_NAMES['node'],
+  TECH_NAMES['html'],
+  TECH_NAMES['css'],
+  TECH_NAMES['django'],
+  TECH_NAMES['express'],
+  TECH_NAMES['git'],
+  TECH_NAMES['postgres']
+].map((key) => KNOWN_TECHS.find((tech) => tech.name === key))
 
-/* todo: fm - delete*/
+/* todo: fm - delete and move into all known techs*/
 export const OTHER_TECHS = [new Tech('Bulma', null, null)]
