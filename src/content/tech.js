7d0e08dd888c845fdc6752ec4e2c59fd6542ca5d
Fearchar MacLean <46941430+Fearchar@users.noreply.github.com>
2021-06-12 14:01:21 +0100

    Refactored tech.js

--- src/content/tech.js
+++ src/content/tech.js
@@ -1,32 +1,60 @@
 class Tech {
   constructor(name, iconName, priority, aboutExclude = false) {
-    ;(this.name = name), (this.iconName = iconName)
+    this.name = name
+    this.iconName = iconName
     this.priority = priority
     this.aboutExclude = aboutExclude
   }
 }
 
-let knownTechs = [
-  new Tech('JavaScript', 'javascript-plain', 10),
-  new Tech('React', 'react-original', 8),
-  new Tech('MongoDB', 'mongodb-plain', 6),
-  new Tech('Mocha', 'mocha-plain', 5),
-  new Tech('Babel', 'babel-plain', 7),
-  new Tech('Webpack', 'webpack-plain', 7.3),
-  new Tech('Python', 'python-plain', 3),
-  new Tech('Node.js', 'nodejs-plain', 7),
-  new Tech('HTML5', 'html5-plain', 9),
-  new Tech('CSS3', 'css3-plain', 9),
-  new Tech('SCSS', 'sass-original', 8),
-  new Tech('Django', 'django-plain', 3),
-  new Tech('Express', 'express-original', 6),
-  new Tech('Git', 'git-plain', 8),
-  new Tech('PostgreSQL', 'postgresql-plain', 3),
-  new Tech('Heroku', 'heroku-original', 5)
-]
+const techNames = {
+  js: 'JavaScript',
+  react: 'React',
+  mongodb: 'MongoDB',
+  mocha: 'Mocha',
+  bable: 'Bable',
+  webpack: 'Webpack',
+  python: 'Python',
+  node: 'Node.js',
+  html: 'HTML5',
+  css: 'CSS3',
+  scss: 'SCSS',
+  django: 'Django',
+  express: 'Express',
+  git: 'Git',
+  postgres: 'PostgreSQL',
+  heroku: 'Heroku'
+}
 
-const otherTechs = [new Tech('Bulma', null, null)]
+/* todo: fm - change to knownTechs*/
+export const allKnownTechs = {
+  [techNames['js']]: new Tech(techNames['js'], 'javascript-plain', 10),
+  [techNames['react']]: new Tech(techNames['react'], 'react-original', 8),
+  [techNames['mongodb']]: new Tech(techNames['mongodb'], 'mongodb-plain', 6),
+  [techNames['mocha']]: new Tech(techNames['mocha'], 'mocha-plain', 5),
+  [techNames['bable']]: new Tech(techNames['bable'], 'babel-plain', 7),
+  [techNames['webpack']]: new Tech(techNames['webpack'], 'webpack-plain', 7.3),
+  [techNames['python']]: new Tech(techNames['python'], 'python-plain', 3),
+  [techNames['node']]: new Tech(techNames['node'], 'nodejs-plain', 7),
+  [techNames['html']]: new Tech(techNames['html'], 'html5-plain', 9),
+  [techNames['css']]: new Tech(techNames['css'], 'css3-plain', 9),
+  [techNames['scss']]: new Tech(techNames['scss'], 'sass-original', 8),
+  [techNames['django']]: new Tech(techNames['django'], 'django-plain', 3),
+  [techNames['express']]: new Tech(techNames['express'], 'express-original', 6),
+  [techNames['git']]: new Tech(techNames['git'], 'git-plain', 8),
+  [techNames['postgres']]: new Tech(
+    techNames['postgres'],
+    'postgresql-plain',
+    3
+  ),
+  [techNames['heroku']]: new Tech(techNames['heroku'], 'heroku-original', 5)
+}
 
-knownTechs = knownTechs.sort((techA, techB) => techB.priority - techA.priority)
+export const knownTechs = Object.keys(allKnownTechs).map(
+  (key) => allKnownTechs[key]
+)
 
-export { Tech, knownTechs, otherTechs }
+export const aboutTechs = [knownTechs['JavaScript']]
+
+/* todo: fm - delete*/
+export const otherTechs = [new Tech('Bulma', null, null)]
